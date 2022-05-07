d6fb46987107970ba12dfc488abd01891c05a152
faked86 <ulturgashev112@gmail.com>
2022-05-07 09:11:02 +0300

    added main logic

--- src/models.py
+++ src/models.py
@@ -5,9 +5,10 @@ class Question(db.Model):
     __tablename__ = 'questions'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text)
+    question = db.Column(db.Text)
     answer = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
+    last_request = db.Column(db.Boolean)
 
     def __repr__(self):
         return f"<question:{self.id}, {self.created_at}>"
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,57 @@
-from flask import request
+from flask import request, jsonify
+import requests
 
-from src import app
+from src import app, db
+from src.models import Question
+
+
+API_URL = "https://jservice.io/api/random?count="
 
 
 @app.post('/questions')
-def hello():
-    return request.json
+def process():
+    num = request.json.get("questions_num")
+    if num is None:
+        return "You need to send JSON with 'questions_num' key", 400
+    if not isinstance(num, int):
+        return "'questions_num' should be integer", 400
+
+    last_quest = Question.query.filter_by(last_request=True).all()
+    results = []
+    try:
+        for res in last_quest:
+            res.last_request = False
+            results.append(
+                {
+                    "id": res.id,
+                    "question": res.question,
+                    "answer": res.answer,
+                    "created_at": res.created_at
+                }
+            )
+        db.session.commit()
+    except Exception as err:
+        print(err)
+        db.session.rollback()
+
+    api_response = requests.get(API_URL + f"{num}")
+    try:
+        for response in api_response.json():
+            while Question.query.filter_by(id=response["id"]).all():
+                print("Same question! Retrying.")
+                response = requests.get(API_URL + "1").json()[0]
+
+            question = Question(
+                id=response["id"],
+                question=response["question"],
+                answer=response["answer"],
+                created_at=response["created_at"],
+                last_request=True,
+            )
+            db.session.add(question)
+        db.session.commit()
+    except Exception as err:
+        print(err)
+        db.session.rollback()
+
+    return jsonify(results)
